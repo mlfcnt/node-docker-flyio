4b7976d78f9d5c7ac5afd81823acb2d9c5a69c57
TMN <t.martin@adsoftware.fr>
2021-05-28 18:28:19 +0200

    test auto deploy

--- server.js
+++ server.js
@@ -2,14 +2,13 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get(['/', '/:name'], (req, res) => {
-  greeting = '<h1>Hello From Node on Fly!</h1>';
-  const name = req.params.name;
-  if (name) {
-    res.send(greeting + '<strong>and hello to </strong> ' + name);
-  } else {
-    res.send(greeting);
-  }
+app.get('/', (req, res) => {
+  res.send('hello');
+});
+
+app.get('/date', (req, res) => {
+  const date = new Date().toLocaleString('fr');
+  res.send(date);
 });
 
 app.listen(port, () => console.log(`HelloNode app listening on port ${port}!`));
